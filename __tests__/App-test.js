9ad04f0d8a9e68b55023c1ca49df22106eb61b52
Jorge Duarte <info@malagadev.com>
2021-05-25 04:04:17 +0200

    chore: lint

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,6 +1,12 @@
-/**
- * @format
- */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                               *
+ *                    | react-native-orderbook |                 *
+ *                                                               *
+ *  License |  MIT General Public License                        *
+ *  Author  |  Jorge Duarte Rodríguez <info@malagadev.com>       *
+ *                                                               *
+ *                            (c) 2021                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 import 'react-native';
 import React from 'react';
