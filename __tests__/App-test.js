e96c3567b0e36555ee9b2c3ab61bc6ebaaaa7381
Jorge Duarte <justhardhead@gmail.com>
2021-05-27 06:27:24 +0200

    test: fixes basic main app instance test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,20 +1,10 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                               *
- *                    | react-native-orderbook |                 *
- *                                                               *
- *  License |  MIT General Public License                        *
- *  Author  |  Jorge Duarte Rodríguez <info@malagadev.com>       *
- *                                                               *
- *                            (c) 2021                           *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 import 'react-native';
 import React from 'react';
-import App from '../src/App';
+import App from '../src/app';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+    renderer.create(<App />);
 });
