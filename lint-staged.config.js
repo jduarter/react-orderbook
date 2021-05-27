81907fb8b09ac85ab5cf58e91eb426e23a7eb89c
Jorge Duarte <justhardhead@gmail.com>
2021-05-27 04:13:41 +0200

    chore: tweaks lint-staged  settings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'src/**/*.js': ['npx eslint --ext .js src/ --fix', 'npm test', 'npm run sec:detect-secrets'],
-    'src/**/*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit', 'npx eslint --ext .tsx --ext .ts src/ --fix', 'npm test', 'npm run sec:detect-secrets'],
+    '*.js': ['npx eslint --ext .js src/ --fix', 'npm test', 'npm run sec:detect-secrets'],
+    '*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit', 'npx eslint --ext .tsx --ext .ts src/ --fix', 'npm test', 'npm run sec:detect-secrets'],
 };
