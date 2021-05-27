a79f9ed1880f5a083b87b708d35a5c918f5b840e
Jorge Duarte <justhardhead@gmail.com>
2021-05-27 04:49:46 +0200

    chore: tweaks cfg files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: ['.js', '.ts', '.tsx', '.json'],
+                alias: {
+                    '@components': './src/components',
+                    '@screens': './src/screens',
+                },
+            },
+        ],
+    ],
 };
