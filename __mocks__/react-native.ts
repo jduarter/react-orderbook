977f898608198699d128d2ff3a7e9c263f7a9a81
Jorge Duarte <justhardhead@gmail.com>
2021-05-27 06:20:06 +0200

    test: adds missing mocks

--- __mocks__/react-native.ts
+++ __mocks__/react-native.ts
@@ -1,17 +1,8 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                               *
- *                    | react-native-orderbook |                 *
- *                                                               *
- *  License |  MIT General Public License                        *
- *  Author  |  Jorge Duarte Rodríguez <info@malagadev.com>       *
- *                                                               *
- *                            (c) 2021                           *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 import { NativeModules } from 'react-native';
 
 NativeModules.RNCNetInfo = {
-  getCurrentConnectivity: jest.fn(),
-  isConnectionMetered: jest.fn(),
-  addListener: jest.fn(),
-  removeListeners: jest.fn(),
+    getCurrentConnectivity: jest.fn(),
+    isConnectionMetered: jest.fn(),
+    addListener: jest.fn(),
+    removeListeners: jest.fn(),
 };
