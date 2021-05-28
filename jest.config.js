5f78032d570211353b13c7a4571c54548ae55ee2
Jorge Duarte <justhardhead@gmail.com>
2021-05-28 11:08:08 +0200

    build: updates jest config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,23 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
-  ...tsjPreset,
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    ...tsjPreset.transform,
-    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-  },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
-  // This is the only part which you can keep
-  // from the above linked tutorial's config:
-  cacheDirectory: '.jest/cache',
+	...tsjPreset,
+	preset: 'react-native',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	transform: {
+		...tsjPreset.transform,
+		'\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+	},
+	testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+	transformIgnorePatterns:['<rootDir>/node_modules/'],
+
+	testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+	globals: {
+		'ts-jest': {
+			babelConfig: true,
+		},
+	},
+	// This is the only part which you can keep
+	// from the above linked tutorial's config:
+	cacheDirectory: '.jest/cache',
 };
