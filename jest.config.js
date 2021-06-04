d0d8172c2d98abf03852de032b98e16618a82327
Jorge Duarte <info@malagadev.com>
2021-06-04 13:05:01 +0200

    lint: linting

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,22 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
-	...tsjPreset,
-	preset: 'react-native',
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	transform: {
-		...tsjPreset.transform,
-		'\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-		"node_modules/react-native-orientation-locker/.+\\.(j|t)sx?$": "ts-jest"
-	},
-	testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-//	transformIgnorePatterns:['<rootDir>/node_modules/react-native-orientation-locker'],
-
-	testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
-	globals: {
-		'ts-jest': {
-			babelConfig: true,
-		},
-	},
-	// This is the only part which you can keep
-	// from the above linked tutorial's config:
-	cacheDirectory: '.jest/cache',
+  ...tsjPreset,
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    'node_modules/react-native-orientation-locker/.+\\.(j|t)sx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  // This is the only part which you can keep
+  // from the above linked tutorial's config:
+  cacheDirectory: '.jest/cache',
 };
