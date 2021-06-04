76a0a70ba011c47070d608ddb6599572146415c2
Jorge Duarte <info@malagadev.com>
2021-06-04 12:57:21 +0200

    fix: fixes tests and useWebSocket unneeded null-check

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -14,14 +14,13 @@ import App from '../app';
 
 afterEach(() => {
   jest.resetAllMocks();
-  jest.clearAllTimers();
 });
 
-it('renders correctly', () => {
+it('renders correctly', async () => {
   const testRenderer = TestRenderer.create(<App />);
   const testInstance = testRenderer.root;
 
-  const mainView = testInstance.findByProps({ testID: 'MAIN_VIEW' });
+  const mainView = await testInstance.findByProps({ testID: 'MAIN_VIEW' });
 
   expect(() =>
     testInstance.findByProps({ testID: 'MAIN_ORDERBOOK_INSTANCE' }),
