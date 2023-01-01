5b73b8dbdc4b0398107dc59983ed295500c48835
Jorge D <info@malagadev.com>
2023-01-01 16:12:28 +0100

    feat: adds support for initial exchange selector

--- src/app.tsx
+++ src/app.tsx
@@ -11,9 +11,12 @@ import Orientation from 'react-native-orientation-locker';
 import { Orderbook } from './screens/Orderbook';
 // @ts-ignore
 import { WEBSOCKET_URI } from 'react-native-config';
-//import Binance from './exchanges/Binance';
-//import CryptoFacilities from './exchanges/CryptoFacilities';
+
+import Binance from './exchanges/Binance';
+import CryptoFacilities from './exchanges/CryptoFacilities';
 import FakeProviderFromJsonFile from './exchanges/FakeProviderFromJsonFile';
+import type { ExchangeModule } from './screens/Orderbook/types';
+import { ExchangeSelector } from './components/ExchangeSelector';
 
 const backgroundStyle = {
   backgroundColor: '#000',
@@ -21,10 +24,31 @@ const backgroundStyle = {
 
 initLoggers();
 
+const EXCHANGES = [
+  { module: FakeProviderFromJsonFile, name: 'FakeProviderFromJsonFile' },
+  { module: Binance, name: 'Binance' },
+  { module: CryptoFacilities, name: 'CryptoFacilities' },
+];
+
 const App: FC = () => {
   React.useEffect(() => {
     Orientation.lockToPortrait();
   }, []);
+
+  const exchangeModule = React.useRef<ExchangeModule | null>(null);
+  const [, forceUpdate] = React.useState<number>(0);
+
+  if (exchangeModule.current === null) {
+    return (
+      <ExchangeSelector
+        exchanges={EXCHANGES}
+        onPress={(module: ExchangeModule) => {
+          exchangeModule.current = module;
+          forceUpdate(Date.now());
+        }}
+      />
+    );
+  }
 
   return (
     <SafeAreaView style={[backgroundStyle, { flex: 1 }]}>
@@ -32,7 +56,7 @@ const App: FC = () => {
 
       <View style={[backgroundStyle, { flex: 1 }]} testID={'MAIN_VIEW'}>
         <Orderbook
-          exchangeModule={FakeProviderFromJsonFile}
+          exchangeModule={exchangeModule.current}
           testID={'MAIN_ORDERBOOK_INSTANCE'}
         />
       </View>
