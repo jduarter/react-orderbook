281dcaf1c2f77e34249e91a3ccfbceb85c4703cc
Jorge D <info@malagadev.com>
2023-01-10 20:10:55 +0100

    style: adds minor styling fixes

--- src/screens/Orderbook/Orderbook.tsx
+++ src/screens/Orderbook/Orderbook.tsx
@@ -145,7 +145,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  groupText: { flex: 1, flexShrink: 0, color: '#fff' },
+  groupText: { flex: 3, flexShrink: 0, color: '#fff' },
   firstColWrap: {
     height: '45%',
     overflow: 'hidden',
